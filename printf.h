5b6987c3b421c92d58af9ae2268872b2dd923ac2
Paul Arden <parden@paul-f4Br8s1.clusters.42paris.fr>
2024-05-25 16:20:20 +0200

    Parser coded =]

--- printf.h
+++ printf.h
@@ -1,12 +1,16 @@
 #define SPECIFIERS "cspdiuxX"
+#define FLAGS "0-+ #"
 
 //pad can be /0 (right-justify), '-' (left-justify), or '0' (pad with zero)
 //sign can be ' ' (no plus sign) or '+' (yes plus sign)
+//prefix can be /0 (no prefix) or '#' (yes prefix)
+//default precision is represented as -1
 typedef struct	s_token
 {
 	char		spec;
 	char		pad;
 	char		sign;
+	char		prefix;
 	int			width;
 	int			precision;
 }				t_token
--- tokenizer.c
+++ tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: parden <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 16:44:37 by parden            #+#    #+#             */
-/*   Updated: 2024/05/24 18:47:16 by parden           ###   ########.fr       */
+/*   Updated: 2024/05/25 16:18:01 by parden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,10 +39,9 @@ static size_t	count_tokens(const char *s)
 		while (s[i] && s[i] != '%')
 			i++;
 		if (s[i] && s[i + 1] != '%')
-		{
 			count++;
+		if (s[i])
 			i += 2;
-		}
 	}
 	return (count);
 }
@@ -52,7 +51,7 @@ static size_t	insert_token(char **token_strs, const char *s, size_t start)
 	size_t	len;
 	
 	len = 0;
-	while (!is_in(s[start + len], SPECIFIERS);
+	while (!is_in(s[start + len], SPECIFIERS))
 			len++;
 	len++;
 	*token_strs = ft_substr(s, start, len);
@@ -82,7 +81,7 @@ static int	tokenizer(char **token_strs, const char *s)
 			i_token_strs++;
 		}
 	}
-	return (1)
+	return (1);
 }
 
 static void	*free_token_list(char **token_strs, t_token **token_list)
@@ -100,9 +99,55 @@ static void	*free_token_list(char **token_strs, t_token **token_list)
 	return (NULL);
 }
 
-t_token	*parse_one(char *token_str)
+t_token	*new_token(const char *token_str, const char token_spec)
 {
-	//TODO
+	t_token	*new;
+
+	new = (t_token *)malloc(sizeof(t_token));
+	if (!new)
+		return (NULL);
+	new->spec = token_spec;
+	new->pad = 0;
+	new->sign = ' ';
+	new->prefix = 0;
+	new->width = 0;
+	new->precision = -1;
+
+	return (new);
+}
+
+
+t_token	*parse_one(const char *token_str)
+{
+	t_token *parsed_token;
+	size_t	token_len;
+	size_t	i;
+
+	token_len = ft_strlen(token_str);
+	parsed_token = new_token(token_str, token_str[token_len - 1]);
+	if (!parsed_token)
+		return (NULL);
+	i = 0;
+	while (is_in(token_str[i], FLAGS))
+	{
+		if (token_str[i] == '#')
+			parsed_token->prefix = '#';
+		if (token_str[i] == '-')
+			parsed_token->pad = '-';
+		if (token_str[i] == '0' && parsed_token->pad != '-')
+			parsed_token->pad = '0';
+		if (token_str[i] == '+')
+			parsed_token->sign = '+';
+		i++;
+	}
+	if (token_str[i] != '.' && ft_atoi(token_str + i))
+		parsed_token->width = ft_atoi(token_str + i);
+	while (ft_isdigit(token_str[i]))
+		i++;
+	if (token_str[i] == '.' && ft_isdigit(token_str[i + 1]))
+		parsed_token->precision = ft_atoi(token_str + i + 1);
+	return (parsed_token);
+	}
 }
 
 t_token	**parser(const char *s)
@@ -110,16 +155,18 @@ t_token	**parser(const char *s)
 	size_t	nb_tok;
 	char	**token_strs;
 	t_token	**token_list;
+	size_t	i;
 
 	nb_tok = count_tokens(s);
-	token_strs = (char **)ft_calloc((nb_tok + 1) * sizeof(char *));
+	token_strs = (char **)ft_calloc((nb_tok + 1), sizeof(char *));
 	if (!token_strs)
 		return (NULL);
 	if (!tokenizer(token_strs, s))
 		return (NULL);
-	token_list = (t_token **)malloc((nb_tok + 1) * sizeof(t_token *));
+	token_list = (t_token **)ft_calloc((nb_tok + 1), sizeof(t_token *));
 	if (!token_list)
 		return ((void *)free_token_strs(token_strs));
+	i = 0;
 	while (token_strs[i])
 	{
 		token_list[i] = parse_one(token_strs[i]);
@@ -130,4 +177,3 @@ t_token	**parser(const char *s)
 	free_token_strs(token_strs);
 	return (token_list);
 }
-
