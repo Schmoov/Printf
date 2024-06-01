291adcfbfdf25849bd9edecb3835dd8cd2a7202a
Paul Arden <parden@paul-f4Br9s1.clusters.42paris.fr>
2024-06-01 15:16:14 +0200

    itoa tested

---
+++ tester_printf/src/printing.memtest.c
@@ -0,0 +1,240 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.memtest.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: parden <parden@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 11:45:29 by parden            #+#    #+#             */
+/*   Updated: 2024/05/31 14:34:38 by parden           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <criterion/criterion.h>
+#include <stdio.h>
+#include "../libftprintf.h"
+
+Test(count_tokens, mandatory)
+{
+	cr_expect_eq(count_tokens("I live sheltered by luxurious ifelse tree forests"), 0);
+	cr_expect_eq(count_tokens("n = %d in block x = %x\nAt address %p"), 3);
+	cr_expect_eq(count_tokens("%c%s%p%d%i%u%x%X"), 8);
+	cr_expect_eq(count_tokens("Qui pour %s le %s pos %%pos%%\nMerde %%%%pos%%%%\nUwU %d,%d\n"), 10);
+	cr_expect_eq(count_tokens("%%%d%%%u%%cs%%p%x%X%%"), 9);
+	cr_expect_eq(count_tokens(""), 0);
+}
+
+
+Test(count_tokens, with_flags)
+{
+	cr_expect_eq(count_tokens("I l---ive 000sheltered by l4354uxuri000ous ifel###se tr#ee forests"), 0);
+	cr_expect_eq(count_tokens("n = % 05d in block x = %#-x\nAt address %-12p"), 3);
+	cr_expect_eq(count_tokens("%-7c%.3s%#0 -+12.7% %014p% 4d%++ +i%u%#x%--00-0--07X"), 9);
+}
+
+Test(tokenize, mandatory)
+{
+	char **token_strs = ft_calloc(1, sizeof(char *));
+	tokenize(token_strs, "I live sheltered by luxurious ifelse tree forests");
+	cr_expect_null(token_strs[0]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(4, sizeof(char *));
+	tokenize(token_strs,"n = %d in block x = %x\nAt address %p");
+	cr_expect_str_eq(token_strs[0], "d");
+	cr_expect_str_eq(token_strs[1], "x");
+	cr_expect_str_eq(token_strs[2], "p");
+	cr_expect_null(token_strs[3]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(9, sizeof(char *));
+	tokenize(token_strs,"%c%s%p%d%i%u%x%X");
+	cr_expect_str_eq(token_strs[0], "c");
+	cr_expect_str_eq(token_strs[1], "s");
+	cr_expect_str_eq(token_strs[2], "p");
+	cr_expect_str_eq(token_strs[3], "d");
+	cr_expect_str_eq(token_strs[4], "i");
+	cr_expect_str_eq(token_strs[5], "u");
+	cr_expect_str_eq(token_strs[6], "x");
+	cr_expect_str_eq(token_strs[7], "X");
+	cr_expect_null(token_strs[8]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(11, sizeof(char *));
+	tokenize(token_strs,"Qui pour %s le %s pos %%pos%%\nMerde %%%%pos%%%%\nUwU %d,%d\n");
+	cr_expect_str_eq(token_strs[0], "s");
+	cr_expect_str_eq(token_strs[1], "s");
+	cr_expect_str_eq(token_strs[2], "%");
+	cr_expect_str_eq(token_strs[3], "%");
+	cr_expect_str_eq(token_strs[4], "%");
+	cr_expect_str_eq(token_strs[5], "%");
+	cr_expect_str_eq(token_strs[6], "%");
+	cr_expect_str_eq(token_strs[7], "%");
+	cr_expect_str_eq(token_strs[8], "d");
+	cr_expect_str_eq(token_strs[9], "d");
+	cr_expect_null(token_strs[10]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(10, sizeof(char *));
+	tokenize(token_strs,"%%%d%%%u%%cs%%p%x%X%%");
+	cr_expect_str_eq(token_strs[0], "%");
+	cr_expect_str_eq(token_strs[1], "d");
+	cr_expect_str_eq(token_strs[2], "%");
+	cr_expect_str_eq(token_strs[3], "u");
+	cr_expect_str_eq(token_strs[4], "%");
+	cr_expect_str_eq(token_strs[5], "%");
+	cr_expect_str_eq(token_strs[6], "x");
+	cr_expect_str_eq(token_strs[7], "X");
+	cr_expect_str_eq(token_strs[8], "%");
+	cr_expect_null(token_strs[9]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(1, sizeof(char *));
+	tokenize(token_strs,"");
+	cr_expect_null(token_strs[0]);
+	free_token_strs(token_strs);
+}
+
+Test(tokenize, with_flags)
+{
+	char **token_strs = ft_calloc(1, sizeof(char *));
+	tokenize(token_strs,"I l---ive 000sheltered by l4354uxuri000ous ifel###se tr#ee forests");
+	cr_expect_null(token_strs[0]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(4, sizeof(char *));
+	tokenize(token_strs,"n = % 05d in block x = %#-x\nAt address %-12p");
+	cr_expect_str_eq(token_strs[0], " 05d");
+	cr_expect_str_eq(token_strs[1], "#-x");
+	cr_expect_str_eq(token_strs[2], "-12p");
+	cr_expect_null(token_strs[3]);
+	free_token_strs(token_strs);
+
+	token_strs = ft_calloc(10, sizeof(char *));
+	tokenize(token_strs,"%-7c%.3s%#0 -+12.7% %014p% 4d%++ +i%u%#x%--00-0--07X");
+	cr_expect_str_eq(token_strs[0], "-7c");
+	cr_expect_str_eq(token_strs[1], ".3s");
+	cr_expect_str_eq(token_strs[2], "#0 -+12.7%");
+	cr_expect_str_eq(token_strs[3], "014p");
+	cr_expect_str_eq(token_strs[4], " 4d");
+	cr_expect_str_eq(token_strs[5], "++ +i");
+	cr_expect_str_eq(token_strs[6], "u");
+	cr_expect_str_eq(token_strs[7], "#x");
+	cr_expect_str_eq(token_strs[8], "--00-0--07X");
+	cr_expect_null(token_strs[9]);
+	free_token_strs(token_strs);
+}
+
+Test(parse, mandatory)
+{
+	t_token **toks = parse("I live sheltered by luxurious ifelse tree forests");
+	cr_expect_null(toks[0]);
+	free_token_list(toks);
+
+	toks = parse("n = %d in block x = %x\nAt address %p");
+	cr_expect_eq(toks[0]->spec, 'd');
+	cr_expect_eq(toks[1]->spec, 'x');
+	cr_expect_eq(toks[2]->spec, 'p');
+	cr_expect_null(toks[3]);
+	free_token_list(toks);
+
+	toks = parse("%c%s%p%d%i%u%x%X");
+	cr_expect_eq(toks[0]->spec, 'c');
+	cr_expect_eq(toks[1]->spec, 's');
+	cr_expect_eq(toks[2]->spec, 'p');
+	cr_expect_eq(toks[3]->spec, 'd');
+	cr_expect_eq(toks[4]->spec, 'i');
+	cr_expect_eq(toks[5]->spec, 'u');
+	cr_expect_eq(toks[6]->spec, 'x');
+	cr_expect_eq(toks[7]->spec, 'X');
+	cr_expect_null(toks[8]);
+	free_token_list(toks);
+
+	toks = parse("Qui pour %s le %s pos %%pos%%\nMerde %%%%pos%%%%\nUwU %d,%d\n");
+	cr_expect_eq(toks[0]->spec, 's');
+	cr_expect_eq(toks[1]->spec, 's');
+	cr_expect_eq(toks[2]->spec, '%');
+	cr_expect_eq(toks[3]->spec, '%');
+	cr_expect_eq(toks[4]->spec, '%');
+	cr_expect_eq(toks[5]->spec, '%');
+	cr_expect_eq(toks[6]->spec, '%');
+	cr_expect_eq(toks[7]->spec, '%');
+	cr_expect_eq(toks[8]->spec, 'd');
+	cr_expect_eq(toks[9]->spec, 'd');
+	cr_expect_null(toks[10]);
+	free_token_list(toks);
+
+	toks = parse("%%%d%%%u%%cs%%p%x%X%%");
+	cr_expect_eq(toks[0]->spec, '%');
+	cr_expect_eq(toks[1]->spec, 'd');
+	cr_expect_eq(toks[2]->spec, '%');
+	cr_expect_eq(toks[3]->spec, 'u');
+	cr_expect_eq(toks[4]->spec, '%');
+	cr_expect_eq(toks[5]->spec, '%');
+	cr_expect_eq(toks[6]->spec, 'x');
+	cr_expect_eq(toks[7]->spec, 'X');
+	cr_expect_eq(toks[8]->spec, '%');
+	cr_expect_null(toks[9]);
+	free_token_list(toks);
+
+	toks = parse("");
+	cr_expect_null(toks[0]);
+	free_token_list(toks);
+}
+
+Test(parse, with_flags)
+{
+	t_token **toks = parse("I l---ive 000sheltered by l4354uxuri000ous ifel###se tr#ee forests");
+	cr_expect_null(toks[0]);
+	free_token_list(toks);
+
+	toks = parse("n = % 05d in block x = %#-x\nAt address %-12p");
+	cr_expect_eq(toks[0]->spec, 'd');
+	cr_expect_eq(toks[0]->pad, '0');
+	cr_expect_eq(toks[0]->sign,' ');
+	cr_expect_eq(toks[0]->width, 5);
+
+	cr_expect_eq(toks[1]->spec, 'x');
+	cr_expect_eq(toks[1]->prefix, '#');
+	cr_expect_eq(toks[1]->pad, '-');
+
+	cr_expect_eq(toks[2]->spec, 'p');
+	cr_expect_eq(toks[2]->pad, '-');
+	cr_expect_eq(toks[2]->width, 12);
+
+	cr_expect_null(toks[3]);
+	free_token_list(toks);
+
+	toks = parse("%-7c%.3s%#0 -+12.7% %014p% 4d%++ +i%u%#x%--00-0--07X");
+	cr_expect_eq(toks[0]->spec, 'c');
+	cr_expect_eq(toks[0]->pad, '-');
+	cr_expect_eq(toks[0]->width, 7);
+
+	cr_expect_eq(toks[1]->spec, 's');
+	cr_expect_eq(toks[1]->precision, 3);
+
+	cr_expect_eq(toks[2]->spec, '%');
+
+	cr_expect_eq(toks[3]->spec, 'p');
+	cr_expect_eq(toks[3]->pad, '0');
+	cr_expect_eq(toks[3]->width, 14);
+
+	cr_expect_eq(toks[4]->spec, 'd');
+	cr_expect_eq(toks[4]->sign, ' ');
+	cr_expect_eq(toks[4]->width, 4);
+	cr_expect_eq(toks[4]->pad, 0);
+
+	cr_expect_eq(toks[5]->spec, 'i');
+	cr_expect_eq(toks[5]->sign, '+');
+
+	cr_expect_eq(toks[6]->spec, 'u');
+
+	cr_expect_eq(toks[7]->spec, 'x');
+	cr_expect_eq(toks[7]->prefix, '#');
+
+	cr_expect_eq(toks[8]->spec, 'X');
+	cr_expect_eq(toks[8]->pad, '-');
+	cr_expect_eq(toks[8]->width, 7);
+	cr_expect_null(toks[9]);
+	free_token_list(toks);
+}
--- trash/trash.c
+++ trash/trash.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
 	int a =43;
 	printf("|% +#-016.14%|\n|12345678901234|");
+	printf("\n%llu\n",UINTPTR_MAX);
 }
